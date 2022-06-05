dd98c7539cfd57bd7f2f20f87db44d6862085e15
MarGotLoose <90410788+margonlo@users.noreply.github.com>
2022-06-05 18:56:34 +0200

    adding pictures for how many game

--- Atenea/Atenea/urls.py
+++ Atenea/Atenea/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from mysite import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
